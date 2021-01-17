c9c7b14bc4a43b64fd30d5662cf4c80ab4019b08
Arko Mandal <arkomandal@Arkos-MacBook-Air.local>
2021-01-17 20:11:10 +0530

    modification

--- db/schema/item.js
+++ db/schema/item.js
@@ -3,14 +3,5 @@ const Schema = mongoose.Schema;
 
 module.exports = {
     item_id: Number,
-    item_name: String,
-    // description: String,
-    // meta: {
-    //     type: String
-    // },
-    // serial_number: { type: String },
-    // parent: { type: Schema.Types.ObjectId }, //to differentiate between category and sub-category
-    // lang_id: { type: Schema.Types.ObjectId },
-    // fid: { type: Schema.Types.ObjectId }, //_id of the item inserted in main language
-    // status: { type: Number, default: 1 }
+    item_name: String
 };
